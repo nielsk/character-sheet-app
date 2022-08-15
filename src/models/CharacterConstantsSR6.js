21e6ad645d2b678ed92077d8ca26f6122a877722
Niels Kobschaetzki <niels@kobschaetzki.net>
2022-08-15 08:17:19 +0200

    constants für sr6

--- src/models/CharacterConstantsSR6.js
+++ src/models/CharacterConstantsSR6.js
@@ -2,40 +2,39 @@
  * Matching attribute full names to the character properties.
  */
 export const attributes = Object.freeze({
+    BODY: 'bod',
     STRENGTH: 'str',
-    DEXTERITY: 'dex',
-    CONSTITUTION: 'con',
-    INTELLIGENCE: 'intel',
-    WISDOM: 'wis',
-    CHARISMA: 'cha'
+    AGILITY: 'agi',
+    REACTION: 'rea',
+    LOGIC: 'log',
+    INTUITION: 'intui',
+    WILLPOWER: 'will',
+    CHARISMA: 'cha',
+    EDGE: 'edge',
+    MAGIC: 'mag',
+    RESONANCE: 'res'
 });
 
 /**
  * Matching skills to attributes.
  */
 export const skillAttributes = Object.freeze({
-    acrobatics: attributes.DEXTERITY,
-    animal_handling: attributes.WISDOM,
-    arcana: attributes.INTELLIGENCE,
-    athletics: attributes.STRENGTH,
-    deception: attributes.CHARISMA,
-    history: attributes.INTELLIGENCE,
-    insight: attributes.WISDOM,
-    intimidation: attributes.CHARISMA,
-    investigation: attributes.INTELLIGENCE,
-    medicine: attributes.WISDOM,
-    nature: attributes.INTELLIGENCE,
-    perception: attributes.WISDOM,
-    performance: attributes.CHARISMA,
-    persuasion: attributes.CHARISMA,
-    religion: attributes.INTELLIGENCE,
-    sleight_of_hand: attributes.DEXTERITY,
-    stealth: attributes.DEXTERITY,
-    survival: attributes.WISDOM
-});
-
-export const skillLevels = Object.freeze({
-    UNSKILLED: 0,
-    PROFICIENT: 1,
-    EXPERT: 2
+    astral: attributes.MAGIC,
+    athletics: attributes.AGILITY,
+    biotech: attributes.LOGIC,
+    close_combat: attributes.AGILITY,
+    con: attributes.CHARISMA,
+    conjuring: attributes.MAGIC,
+    cracking: attributes.LOGIC,
+    electronics: attributes.LOGIC,
+    engineering: attributes.LOGIC,
+    exotic_weapons: attributes.AGILITY,
+    firearms: attributes.AGILITY,
+    influence: attributes.CHARISMA,
+    outdoors: attributes.INTUITION,
+    perception: attributes.INTUITION,
+    piloting: attributes.REACTION,
+    sorcery: attributes.MAGIC,
+    stealth: attributes.AGILITY,
+    tasking: attributes.RESONANCE
 });
